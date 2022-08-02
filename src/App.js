7f0e6d2f3a7a20868f3983506cb4d49ff9876de6
Engines-screaming <karl.ravago@gmail.com>
2022-08-01 20:59:46 -0700

    center the eightball on page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Eightball from './Eightball'
+import './App.css'
+
+function App() {
+    return (
+        <div className="container">
+            <Eightball />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Eightball from './Eightball';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Eightball />
+  <App />
 );
