54f1c459ed380f007130a343086671ca9af74f5e
Engines-screaming <karl.ravago@gmail.com>
2022-08-01 20:26:51 -0700

    create the eightball component

---
+++ src/Eightball.js
@@ -0,0 +1,11 @@
+import './Eightball.css';
+
+function Eightball() {
+  return (
+    <div className="Eightball">
+      <p className="Eightball-text">Think of a question</p>
+    </div>
+  );
+}
+
+export default Eightball;
