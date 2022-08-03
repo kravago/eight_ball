cf02dd614cd8265271b135e6f67e58746fddac96
Engines-screaming <karl.ravago@gmail.com>
2022-08-02 22:15:54 -0700

    eightball works when clicked

--- src/Eightball.js
+++ src/Eightball.js
@@ -1,9 +1,26 @@
 import './Eightball.css';
+import { useState } from 'react';
+import answerBank from './answerBank';
 
 function Eightball() {
+  const [ color, setColor ] = useState('black');
+  const [ msg, setMsg ] = useState('think of a question');
+
+  const generateAnswer = (arr) => {
+    const randIdx = Math.floor(Math.random() * arr.length);
+    return arr[randIdx];
+  }
+
+  const handleClick = (e) => {
+    // e.target.style.backgroundColor = "red"
+    const { msg: message, color: bgColor } = generateAnswer(answerBank);
+    setColor(bgColor);
+    setMsg(message);
+  }
+
   return (
-    <div className="Eightball">
-      <p className="Eightball-text">Think of a question</p>
+    <div className="Eightball" style={{backgroundColor: color}} onClick={handleClick}>
+      <p className="Eightball-text">{msg}</p>
     </div>
   );
 }
